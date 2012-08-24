b27fc3baa3bffd408ee73f7d5b25f77dbe216cbb
Alex Xandra Albert Sim <bertzzie@gmail.com>
2012-08-24 13:50:39 +0700

    Dynamic Menu

---
+++ PMI/Controllers/NavigationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PMI.Models;
+
+namespace PMI.Controllers
+{
+    [ChildActionOnly]
+    public class NavigationController : Controller
+    {
+        private List<MenuItem> MainMenu = MenuItem.MainMenu();
+        private List<MenuItem> AdminMenu = MenuItem.AdminMenu();
+
+        public PartialViewResult Menu(string controller, string action)
+        {
+            var menu = MainMenu;
+            menu.Add(AuthMenu());
+            if (User.IsInRole("CanPostNews"))
+                menu.AddRange(AdminMenu);
+
+            menu.Where(p => p.Action == action && p.Controller == controller)
+                .Select(p => { p.Active = true; return p; })
+                .ToList(); // to activate the lazy evaluation
+            return PartialView(menu);
+        }
+
+        private MenuItem AuthMenu()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return new MenuItem { Text = "Logout", Controller = "Account", Action = "LogOff", Area = "", Active = false };
+            }
+            else
+            {
+                return new MenuItem { Text = "Login", Controller = "Account", Action = "LogOn", Area = "", Active = false };
+            }
+        }
+    }
+}
---
+++ PMI/Models/MenuItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PMI.Models
+{
+    public class MenuItem
+    {
+        public string Text { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string Area { get; set; }
+        public bool Active { get; set; }
+
+        public static List<MenuItem> MainMenu()
+        {
+            var mainMenu = new List<MenuItem>();
+            mainMenu.Add(new MenuItem { Text = "Berita", Controller = "Home", Action = "Index", Active = false, Area = "" });
+            mainMenu.Add(new MenuItem { Text = "Partisipasi", Controller = "Home", Action = "About", Active = false, Area = "" });
+            mainMenu.Add(new MenuItem { Text = "Program", Controller = "Home", Action = "About", Active = false, Area = "" });
+            mainMenu.Add(new MenuItem { Text = "Informasi", Controller = "Home", Action = "About", Active = false, Area = "" });
+            mainMenu.Add(new MenuItem { Text = "Komunitas", Controller = "Home", Action = "About", Active = false, Area = "" });
+
+            return mainMenu;
+        }
+
+        public static List<MenuItem> AdminMenu()
+        {
+            var adminMenu = new List<MenuItem>();
+            adminMenu.Add(new MenuItem { Text = "Tulisan", Controller = "Post", Action = "Index", Active = false, Area = "Admin" });
+            adminMenu.Add(new MenuItem { Text = "Kategori", Controller = "Category", Action = "Index", Active = false, Area = "Admin" });
+
+            return adminMenu;
+        }
+    }
+}
