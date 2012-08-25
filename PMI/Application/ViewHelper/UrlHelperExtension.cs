ffc6798c34951b06c4f1a551fc6bb50d047425c4
Alex Xandra Albert Sim <bertzzie@gmail.com>
2012-08-25 13:13:01 +0700

    CSS on Themes worked!

---
+++ PMI/Application/ViewHelper/UrlHelperExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace PMI.Application.ViewHelper
+{
+    public static class UrlHelperExtension
+    {
+        public static string ThemeContent(this UrlHelper url, string path)
+        {
+            var theme = WebConfigurationManager.AppSettings["webpages:Theme"];
+            var finalPath = "~/Themes/";
+
+            if (string.IsNullOrEmpty(theme))
+                finalPath = finalPath + "Default/";
+            else
+                finalPath = finalPath + theme + "/";
+
+            // path cleaning
+            if (path.StartsWith("~/"))
+                path = path.Remove(0, 2);
+
+            if (path.StartsWith("/"))
+                path = path.Remove(0, 1);
+
+            path = path.Replace("../", string.Empty);
+            finalPath = finalPath + path;
+
+            return VirtualPathUtility.ToAbsolute(finalPath);
+        }
+    }
+}
