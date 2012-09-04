ff92bb412931397a2a866b3aea5b266a01760df8
Alex Xandra Albert Sim <bertzzie@gmail.com>
2012-09-04 19:04:04 +0700

    SiteInfo internationalization

--- PMI/Models/SiteInfoModel.cs
+++ PMI/Models/SiteInfoModel.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PMI.Resources.Model;
 
 namespace PMI.Models
 {
@@ -15,13 +16,13 @@ namespace PMI.Models
 
     internal class SiteInfoMetadata
     {
-        [Required(ErrorMessage = "Tema harus dipilih!")]
-        [DisplayName("Tema")]
+        [Required(ErrorMessageResourceName = "ThemeError", ErrorMessageResourceType = typeof(SiteInfoResources))]
+        [Display(Name = "Theme", ResourceType = typeof(SiteInfoResources))]
         public string theme { get; set; }
 
         [AllowHtml]
-        [Required(ErrorMessage = "Footer harus diisi!")]
-        [DisplayName("Footer Situs")]
+        [Required(ErrorMessageResourceName = "FooterError", ErrorMessageResourceType = typeof(SiteInfoResources))]
+        [Display(Name = "Footer", ResourceType = typeof(SiteInfoResources))]
         public string footer { get; set; }
     }
 }
